54ef73b357cb4ee14cc5c2fdb7fe6ca3c5dbfe9a
Lucas Marques <Colten610@gmail.com>
2024-09-11 17:08:19 -0300

    initial commit

---
+++ Website/WFT/src/i18n/pt.js
@@ -0,0 +1,5 @@
+export default {
+
+    WhatForToday: "O que tem pra hoje?",
+
+}
---
+++ Website/WFT/src/i18n/strings.js
@@ -0,0 +1,7 @@
+// import LocalizedStrings from 'react-localization';
+import enI18n from './en';
+import ptI18n from './pt';
+
+export default strings = new LocalizedStrings({
+  pt: ptI18n,
+});
